ba3ac7559c701240261e705f87e201c0a01ac1ef
Matt Broadstone <mbroadstone@devonit.com>
2014-03-02 16:46:50 -0500

    changes per-jshint suggestions

--- tests/context.js
+++ tests/context.js
@@ -1,116 +1,103 @@
-var fs = require('fs');
-var http = require('http');
-var async = require('async');
-var express = require('express');
-var request = require('request');
-
-var rest = require('../lib');
-var User = require('./lib/user.js');
-var Address = require('./lib/address.js');
-
-var switchboard = {};
+var fs = require('fs'),
+    http = require('http'),
+    async = require('async'),
+    express = require('express'),
+    request = require('request'),
+    rest = require('../lib'),
+    User = require('./lib/user.js'),
+    Address = require('./lib/address.js'),
+    switchboard = {};
 
 var truncate = function(callback) {
-
-	async.forEach([User, Address], function(model, callback) {
-
-		model.findAll()
-			.success(function(instances) {
-				async.forEach(instances, function(instance, callback) {
-					instance.destroy();
-					callback();
-				},
-				function() {
-					callback();
-				});
-			})
-	},
-	function() {
-		switchboard.server.close(callback);
-	});
+  async.forEach([User, Address], function(model, callback) {
+    model.findAll()
+      .success(function(instances) {
+        async.forEach(instances, function(instance, callback) {
+          instance.destroy();
+          callback();
+        },
+        function() {
+          callback();
+        });
+      })
+  },
+  function() {
+    switchboard.server.close(callback);
+  });
 };
 
 module.exports = {
+  prefetch: function(test) {
+    request.post({
+      url: "http://localhost:48281/users",
+      json: { username: "jamez", email: "jamez@gmail.com" }
 
-	prefetch: function(test) {
-		request.post({
-			url: "http://localhost:48281/users",
-			json: { username: "jamez", email: "jamez@gmail.com" }
+    }, function(err, response, body) {
+      if (err) console.log("ERROR: " + err);
+      var path = response.headers['location'];
+      request.get({ url: "http://localhost:48281" + path }, function(err, response, body) {
+        var record = typeof body == 'string' ? JSON.parse(body) : body;
+        delete record.id;
 
-		}, function(err, response, body) {
+        var user = { "username": "mocked", "email": "mocked@gmail.com" };
 
-			if (err) console.log("ERROR: " + err);
+        test.equal(response.statusCode, 200, "get back results");
+        test.deepEqual(record, user, "skipped fetching instance");
+        test.done();
+      });
+    });
+  },
 
-			var path = response.headers['location'];
+  multipleStarts: function(test) {
+    var users = switchboard.users;
+    var startCount;
 
-			request.get({ url: "http://localhost:48281" + path }, function(err, response, body) {
+    users.read.start(function(req, res, context) {
+      startCount = 1;
+      return context.continue();
+    });
 
-				var record = typeof body == 'string' ? JSON.parse(body) : body;
-				delete record.id;
+    users.read.start(function(req, res, context) {
+      startCount++;
+      return context.continue();
+    });
 
-				var user = { "username": "mocked", "email": "mocked@gmail.com" };
+    request.get({ url: "http://localhost:48281/users/1" }, function(err, response, body) {
+      test.equal(startCount, 2);
+      test.done();
+    });
+  },
 
-				test.equal(response.statusCode, 200, "get back results");
-				test.deepEqual(record, user, "skipped fetching instance");
+  setUp: function(callback) {
+    var app = express();
+    app.use(express.json());
+    app.use(express.urlencoded());      
 
-				test.done();
-			});
-		});
-	},
+    User.hasMany(Address);
 
-	multipleStarts: function(test) {
+    User.sync();
+    Address.sync();
 
-		var users = switchboard.users;
+    rest.initialize({ app: app });
 
-		var startCount;
+    var users = rest.resource({
+      model: User,
+      endpoints: ['/users', '/users/:id']
+    });
 
-		users.read.start(function(req, res, context) {
-			startCount = 1;
-			return context.continue();
-		});
+    users.read.fetch.before(function(req, res, context) {
+      context.instance = { username: "mocked", email: "mocked@gmail.com" };
+      return context.skip();
+    });
 
-		users.read.start(function(req, res, context) {
-			startCount++;
-			return context.continue();
-		});
+    switchboard.users = users;
+    switchboard.server = http.createServer(app);
+    switchboard.server.listen(48281, null, null, callback);
+  },
 
-		request.get({ url: "http://localhost:48281/users/1" }, function(err, response, body) {
-			test.equal(startCount, 2);
-			test.done();
-		});
-	},
-
-	setUp: function(callback) {
-
-		var app = express();
-		app.use(express.bodyParser());
-
-		User.hasMany(Address);
-
-		User.sync();
-		Address.sync();
-
-		rest.initialize({ app: app });
-
-		var users = rest.resource({
-			model: User,
-			endpoints: ['/users', '/users/:id']
-		});
-
-		users.read.fetch.before(function(req, res, context) {
-			context.instance = { username: "mocked", email: "mocked@gmail.com" };
-			return context.skip();
-		});
-
-		switchboard.users = users;
-
-		switchboard.server = http.createServer(app);
-		switchboard.server.listen( 48281, null, null, function() { setTimeout(callback, 1000) });
-	},
-
-	tearDown: function(callback) {
-		truncate(callback);
-	}
+  tearDown: function(callback) {
+    truncate(callback);
+  }
 };
 
 
--- tests/resource.js
+++ tests/resource.js
@@ -1,204 +1,191 @@
-var fs = require('fs');
-var http = require('http');
-var async = require('async');
-var express = require('express');
-var request = require('request');
-
-var rest = require('../lib');
-var User = require('./lib/user.js');
-var Address = require('./lib/address.js');
-
-var switchboard = {};
+var fs = require('fs'),
+  http = require('http'),
+  async = require('async'),
+  express = require('express'),
+  request = require('request'),
+  rest = require('../lib'),
+  User = require('./lib/user.js'),
+  Address = require('./lib/address.js'),
+  switchboard = {};
 
 var truncate = function(callback) {
-
-	async.forEach([User, Address], function(model, callback) {
-
-		model.findAll()
-			.success(function(instances) {
-				async.forEach(instances, function(instance, callback) {
-					instance.destroy();
-					callback();
-				},
-				function() {
-					callback();
-				});
-			})
-	},
-	function() {
-		switchboard.server.close(callback);
-	});
+  async.forEach([User, Address], function(model, callback) {
+    model.findAll()
+      .success(function(instances) {
+        async.forEach(instances, function(instance, callback) {
+          instance.destroy();
+          callback();
+        },
+        function() {
+          callback();
+        });
+      })
+  }, function() {
+    switchboard.server.close(callback);
+  });
 };
 
 module.exports = {
+  create: function(test) {
+    request.post({
+      url: "http://localhost:48281/users",
+      json: { username: "arthur", email: "arthur@gmail.com" }
 
-	create: function(test) {
-		request.post({
-			url: "http://localhost:48281/users",
-			json: { username: "arthur", email: "arthur@gmail.com" }
+    }, function(err, response, body) {
 
-		}, function(err, response, body) {
+      if (err) console.log("ERROR: " + err);
 
-			if (err) console.log("ERROR: " + err);
+      test.equal(response.statusCode, 201, "we get a created 201 for creating an entry");
+      test.ok(response.headers['location'].match(/\/users\/\d+/), "we get plausible location header");
+      test.done();
+    });
+  },
 
-			test.equal(response.statusCode, 201, "we get a created 201 for creating an entry");
-			test.ok(response.headers['location'].match(/\/users\/\d+/), "we get plausible location header");
-			test.done();
-		});
-	},
+  read: function(test) {
+    request.post({
+      url: "http://localhost:48281/users",
+      json: { username: "jamez", email: "jamez@gmail.com" }
+    }, function(err, response, body) {
+      if (err)
+        console.log("ERROR: " + err);
 
-	read: function(test) {
-		request.post({
-			url: "http://localhost:48281/users",
-			json: { username: "jamez", email: "jamez@gmail.com" }
+      var path = response.headers['location'];      
+      request.get({ url: "http://localhost:48281" + path }, function(err, response, body) {
 
-		}, function(err, response, body) {
+        var record = typeof body == 'string' ? JSON.parse(body) : body;
+        delete record.id;
 
-			if (err) console.log("ERROR: " + err);
+        var user = { "username": "jamez", "email": "jamez@gmail.com" };
 
-			var path = response.headers['location'];
+        test.equal(response.statusCode, 200, "get back results");
+        test.deepEqual(record, user, "we created one user successfully");
 
-			request.get({ url: "http://localhost:48281" + path }, function(err, response, body) {
+        test.done();
+      });
+    });
+  },
 
-				var record = typeof body == 'string' ? JSON.parse(body) : body;
-				delete record.id;
+  list: function(test) {
 
-				var user = { "username": "jamez", "email": "jamez@gmail.com" };
+    var list = [
+      { username: "arthur", email: "arthur@gmail.com" },
+      { username: "james", email: "james@gmail.com" },
+      { username: "henry", email: "henry@gmail.com" },
+      { username: "william", email: "william@gmail.com" },
+      { username: "edward", email: "edward@gmail.com" }
+    ];
 
-				test.equal(response.statusCode, 200, "get back results");
-				test.deepEqual(record, user, "we created one user successfully");
+    async.forEachSeries(
+      list,
+      function(user, callback) {
+        request.post({
+          url: "http://localhost:48281/users",
+          json: user
+        },
+        function(err, response, body) {
+          callback();
+        });
+      },
+      function(err) {
 
-				test.done();
-			});
-		});
-	},
+        async.parallel([
+          function(callback) {
+            request.get({ url: "http://localhost:48281/users" }, function(err, response, body) {
+              test.equal(response.statusCode, 200, "list comes back okay");
+              var records = JSON.parse(body).map(function(r) { delete r.id; return r });
+              test.deepEqual(records, list, "list gets back what we put in");
+              test.equal(response.headers['content-range'], 'items 0-4/5', "list content range checks out");
+              callback();
+            });
+          },
+          function(callback) {
+            request.get({ url: "http://localhost:48281/users?offset=1&count=2" }, function(err, response, body) {
+              test.equal(200, response.statusCode, "paginated list okay");
+              var records = JSON.parse(body).map(function(r) { delete r.id; return r });
+              test.deepEqual(records, list.slice(1,3), "paginated list gets right elements");
+              test.equal(response.headers['content-range'], 'items 1-2/5', "paginated list content range okay");
+              callback();
+            });
+          }
+        ],
+        function(err) {
+          test.done();
+        });
+      });
+  },
 
-	list: function(test) {
+  update: function(test) {
+    request.post({
+      url: "http://localhost:48281/users",
+      json: { username: "emma", email: "emma@gmail.com" }
+    }, function(err, response, body) {
+      if (err) console.log("ERROR: " + err);
 
-		var list = [
-			{ username: "arthur", email: "arthur@gmail.com" },
-			{ username: "james", email: "james@gmail.com" },
-			{ username: "henry", email: "henry@gmail.com" },
-			{ username: "william", email: "william@gmail.com" },
-			{ username: "edward", email: "edward@gmail.com" }
-		];
+      var location = response.headers['location'];
 
-		async.forEachSeries(
-			list,
-			function(user, callback) {
-				request.post({
-					url: "http://localhost:48281/users",
-					json: user
-				},
-				function(err, response, body) {
-					callback();
-				});
-			},
-			function(err) {
+      request.post({
+        url: "http://localhost:48281" + location,
+        json: { email: "emma@fmail.co.uk" }
+      }, function(err, response, body) {
+        if (err) console.log("ERROR: " + err);
 
-				async.parallel([
-					function(callback) {
-						request.get({ url: "http://localhost:48281/users" }, function(err, response, body) {
-							test.equal(response.statusCode, 200, "list comes back okay");
-							var records = JSON.parse(body).map(function(r) { delete r.id; return r });
-							test.deepEqual(records, list, "list gets back what we put in");
-							test.equal(response.headers['content-range'], 'items 0-4/5', "list content range checks out");
-							callback();
-						});
-					},
-					function(callback) {
-						request.get({ url: "http://localhost:48281/users?offset=1&count=2" }, function(err, response, body) {
-							test.equal(200, response.statusCode, "paginated list okay");
-							var records = JSON.parse(body).map(function(r) { delete r.id; return r });
-							test.deepEqual(records, list.slice(1,3), "paginated list gets right elements");
-							test.equal(response.headers['content-range'], 'items 1-2/5', "paginated list content range okay");
-							callback();
-						});
-					}
-				],
-				function(err) {
-					test.done();
-				});
-			});
-	},
+        var record = typeof body == 'string' ? JSON.parse(body) : body;
+        delete record.id;
 
-	update: function(test) {
-		request.post({
-			url: "http://localhost:48281/users",
-			json: { username: "emma", email: "emma@gmail.com" }
+        var user = { "username": "emma", "email": "emma@fmail.co.uk" };
+        test.equal(response.statusCode, 200, "updating gets successful status");
+        test.deepEqual(record, user, "updated user is updated");
+        test.done();
+      });
+    });
+  },
 
-		}, function(err, response, body) {
+  delete: function(test) {
+    request.post({
+      url: "http://localhost:48281/users",
+      json: { username: "chicken", email: "chicken@gmail.com" }
 
-			if (err) console.log("ERROR: " + err);
+    }, function(err, response, body) {
 
-			var location = response.headers['location'];
+      if (err) console.log("ERROR: " + err);
 
-			request.post({
-				url: "http://localhost:48281" + location,
-				json: { email: "emma@fmail.co.uk" }
+      var location = response.headers['location'];
 
-			}, function(err, response, body) {
+      request.del({ url: "http://localhost:48281" + location }, function(err, response, body) {
 
-				var record = typeof body == 'string' ? JSON.parse(body) : body;
-				delete record.id;
+        test.equal(response.statusCode, 200, "deleting a user gets successful status");
 
-				var user = { "username": "emma", "email": "emma@fmail.co.uk" };
+        request.get({ url: "http://localhost:48281" + location }, function(err, response, body) {
 
-				test.equal(response.statusCode, 200, "updating gets successful status");
-				test.deepEqual(record, user, "updated user is updated");
+          test.equal(response.statusCode, 404, "deleted user is deleted");
+          test.done();
+        });
+      });
+    });
+  },
 
-				test.done();
-			});
-		});
-	},
+  setUp: function(callback) {
+    app = express();
+    app.use(express.json());
+    app.use(express.urlencoded());  
 
-	delete: function(test) {
-		request.post({
-			url: "http://localhost:48281/users",
-			json: { username: "chicken", email: "chicken@gmail.com" }
+    User.hasMany(Address);
 
-		}, function(err, response, body) {
+    User.sync();
+    Address.sync();
 
-			if (err) console.log("ERROR: " + err);
+    rest.initialize({ app: app });
 
-			var location = response.headers['location'];
+    rest.resource({
+      model: User,
+      endpoints: ['/users', '/users/:id']
+    });
 
-			request.del({ url: "http://localhost:48281" + location }, function(err, response, body) {
+    switchboard.server = http.createServer(app);
+    switchboard.server.listen(48281, null, null, callback);
+  },
 
-				test.equal(response.statusCode, 200, "deleting a user gets successful status");
-
-				request.get({ url: "http://localhost:48281" + location }, function(err, response, body) {
-
-					test.equal(response.statusCode, 404, "deleted user is deleted");
-					test.done();
-				});
-			});
-		});
-	},
-
-	setUp: function(callback) {
-
-		var app = express();
-		app.use(express.bodyParser());
-
-		User.hasMany(Address);
-
-		User.sync();
-		Address.sync();
-
-		rest.initialize({ app: app });
-
-		rest.resource({
-			model: User,
-			endpoints: ['/users', '/users/:id']
-		});
-
-		switchboard.server = http.createServer(app);
-		switchboard.server.listen( 48281, null, null, function() { setTimeout(callback, 1000) });
-	},
-
-	tearDown: function(callback) {
-		truncate(callback);
-	}
+  tearDown: function(callback) {
+    truncate(callback);
+  }
 };
-
