c3f075862023b6f27c33fd3c4cd4af6647e725ad
Matt Broadstone <mbroadstone@devonit.com>
2014-04-01 18:04:51 -0400

    added new test classes to improve test coverage

---
+++ tests/epilogue.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var epilogue = require('../lib'),
+    expect = require('chai').expect;
+
+describe('Epilogue', function() {
+
+  it('should throw an exception when initialized without arguments', function(done) {
+    expect(epilogue.initialize).to.throw('please specify an app');
+    done();
+  });
+
+  it('should throw an exception with an invalid updateMethod', function(done) {
+    expect(epilogue.initialize.bind(epilogue, {
+      app: {},
+      updateMethod: "dogs"
+    })).to.throw('updateMethod must be one of PUT, POST, or PATCH');
+    done();
+  });
+
+});
---
+++ tests/resource.updatemethod.js
@@ -0,0 +1,163 @@
+"use strict";
+
+var express = require('express'),
+    request = require('request'),
+    http = require('http'),
+    expect = require('chai').expect,
+    Sequelize = require('sequelize'),
+    _ = require('lodash'),
+    rest = require('../lib');
+
+var test = {};
+describe('Resource(updateMethod)', function() {
+  before(function() {
+    test.db = new Sequelize('main', null, null, {
+      dialect: 'sqlite',
+      logging: false
+    });
+
+    test.User = test.db.define('users', {
+      id:       { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      username: { type: Sequelize.STRING, unique: true },
+      email:    { type: Sequelize.STRING, unique: true, validate: { isEmail: true } }
+    }, {
+      underscored: true,
+      timestamps: false
+    });
+  });
+
+  beforeEach(function(done) {
+    test.db
+      .sync({ force: true })
+      .success(function() {
+        test.app = express();
+        test.app.use(express.json());
+        test.app.use(express.urlencoded());
+        test.server = http.createServer(test.app);
+        test.server.listen(48281, null, null, function() {
+          test.baseUrl =
+            'http://' + test.server.address().address + ':' + test.server.address().port;
+          done();
+        });
+      });
+  });
+
+  afterEach(function(done) {
+    test.db
+      .getQueryInterface()
+      .dropAllTables()
+      .success(function() {
+        test.server.close(done);
+      });
+  });
+
+  // TESTS
+  describe('patch', function() {
+    it('should allow for PATCH as an update method', function(done) {
+      rest.initialize({
+        app: test.app,
+        updateMethod: 'patch',
+      });
+
+      rest.resource({
+        model: test.User,
+        endpoints: ['/users', '/users/:id']
+      });
+
+      var userData = { username: 'jamez', email: 'jamez@gmail.com' };
+      request.post({
+        url: test.baseUrl + '/users',
+        json: userData
+      }, function(error, response, body) {
+        expect(error).is.null;
+        expect(response.headers.location).is.not.empty;
+        var path = response.headers.location;
+        request.patch({
+          url: test.baseUrl + path,
+          json: { email: 'emma@fmail.co.uk' }
+        }, function(err, response, body) {
+          expect(response.statusCode).to.equal(200);
+          var record = _.isObject(body) ? body: JSON.parse(body);
+
+          delete record.id;
+          userData.email = 'emma@fmail.co.uk';
+          expect(record).to.eql(userData);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('put', function() {
+    it('should allow for PATCH as an update method', function(done) {
+      rest.initialize({
+        app: test.app,
+        updateMethod: 'put',
+      });
+      
+      rest.resource({
+        model: test.User,
+        endpoints: ['/users', '/users/:id']
+      });
+
+      var userData = { username: 'jamez', email: 'jamez@gmail.com' };
+      request.post({
+        url: test.baseUrl + '/users',
+        json: userData
+      }, function(error, response, body) {
+        expect(error).is.null;
+        expect(response.headers.location).is.not.empty;
+        var path = response.headers.location;
+        request.put({
+          url: test.baseUrl + path,
+          json: { email: 'emma@fmail.co.uk' }
+        }, function(err, response, body) {
+          expect(response.statusCode).to.equal(200);
+          var record = _.isObject(body) ? body: JSON.parse(body);
+
+          delete record.id;
+          userData.email = 'emma@fmail.co.uk';
+          expect(record).to.eql(userData);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('post', function() {
+    it('should allow for PATCH as an update method', function(done) {
+      rest.initialize({
+        app: test.app,
+        updateMethod: 'post',
+      });
+      
+      rest.resource({
+        model: test.User,
+        endpoints: ['/users', '/users/:id']
+      });
+
+      var userData = { username: 'jamez', email: 'jamez@gmail.com' };
+      request.post({
+        url: test.baseUrl + '/users',
+        json: userData
+      }, function(error, response, body) {
+        expect(error).is.null;
+        expect(response.headers.location).is.not.empty;
+        var path = response.headers.location;
+        request.post({
+          url: test.baseUrl + path,
+          json: { email: 'emma@fmail.co.uk' }
+        }, function(err, response, body) {
+          expect(response.statusCode).to.equal(200);
+          var record = _.isObject(body) ? body: JSON.parse(body);
+
+          delete record.id;
+          userData.email = 'emma@fmail.co.uk';
+          expect(record).to.eql(userData);
+          done();
+        });
+      });
+    });
+  });
+
+});
