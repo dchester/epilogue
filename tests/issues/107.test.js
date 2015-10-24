6f09babc2c978336f731d912af078011c569239a
Matt Broadstone <mbroadst@gmail.com>
2015-10-24 13:47:22 -0400

    test(issue107): add test for issue #107

---
+++ tests/issues/107.test.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var Promise = require('bluebird'),
+    request = require('request'),
+    expect = require('chai').expect,
+    _ = require('lodash'),
+    rest = require('../../lib'),
+    test = require('../support');
+
+describe('issue 107', function() {
+  before(function() {
+    test.models.Channel = test.db.define('Channel', { name: test.Sequelize.STRING }, { timestamps: false, });
+    test.models.AdSlot = test.db.define('AdSlot', { name: test.Sequelize.STRING }, { timestamps: false, });
+    test.models.Channel.hasMany(test.models.AdSlot, { onDelete: 'cascade', hooks: true });
+    test.models.AdSlot.belongsTo(test.models.Channel);
+  });
+
+  beforeEach(function(done) {
+    test.initializeDatabase(function() {
+      test.initializeServer(function() {
+        rest.initialize({ app: test.app, sequelize: test.Sequelize });
+
+        test.channelResource = rest.resource({
+          model: test.models.Channel,
+          associations: true,
+          attributes: ['id','name'],
+          endpoints: ['/api/channels', '/api/channels/:id']
+        });
+
+        return Promise.all([
+            test.models.Channel.create({ name: 'testChannel' }),
+            test.models.AdSlot.create({ name: 'testAd' })
+          ])
+          .spread(function(channel, ad) {
+            return Promise.all([
+              channel.setAdSlots([ ad ]), ad.setChannel(channel)
+            ]);
+          })
+          .then(function() { done(); });
+      });
+    });
+  });
+
+  afterEach(function(done) {
+    test.clearDatabase(function() { test.server.close(done); });
+  });
+
+  it('list should work', function(done) {
+    request.get({
+      url: test.baseUrl + '/api/channels'
+    }, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      var result = _.isObject(body) ? body : JSON.parse(body);
+      expect(result).to.eql([ {
+        id: 1, name: 'testChannel',
+        AdSlots: [ { id: 1, name: 'testAd', ChannelId: 1  } ]
+      } ]);
+
+      done();
+    });
+  });
+
+  it('list should work on associated data', function(done) {
+    request.get({
+      url: test.baseUrl + '/api/channels/1/adslots'
+    }, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      var result = _.isObject(body) ? body : JSON.parse(body);
+      expect(result).to.eql([ { id: 1, name: 'testAd', ChannelId: 1 } ]);
+      done();
+    });
+  });
+
+  it('read should work', function(done) {
+    request.get({
+      url: test.baseUrl + '/api/channels/1'
+    }, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      var result = _.isObject(body) ? body : JSON.parse(body);
+      expect(result).to.eql({
+        id: 1, name: 'testChannel',
+        AdSlots: [ { id: 1, name: 'testAd', ChannelId: 1  } ]
+      });
+
+      done();
+    });
+  });
+
+  it('read should work on associated data', function(done) {
+    request.get({
+      url: test.baseUrl + '/api/channels/1/adslots/1'
+    }, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      var result = _.isObject(body) ? body : JSON.parse(body);
+      expect(result).to.eql({ id: 1, name: 'testAd', ChannelId: 1 });
+      done();
+    });
+  });
+
+});
